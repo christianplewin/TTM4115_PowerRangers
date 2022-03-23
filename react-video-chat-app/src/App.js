c563f3c373d2e6d9342cbf47d19cc7a8405bb851
Christian <chrislew@stud.ntnu.no>
2022-03-23 15:47:39 +0100

    #2 Connect to Firebase

--- react-video-chat-app/src/App.js
+++ react-video-chat-app/src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from "firebase";
+import { firestore } from "firebase/firestore";
+import FirebaseTest from "./FirebaseTest";
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  });
+}
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +32,8 @@ function App() {
         >
           Learn React
         </a>
+
+        {<FirebaseTest />}
       </header>
     </div>
   );
