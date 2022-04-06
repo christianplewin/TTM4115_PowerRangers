f5307ad117047446d133a4c17585e90a01f075a4
Christian <chrislew@stud.ntnu.no>
2022-04-06 13:55:26 +0200

    #7 Clean up code. Add CSS

--- react-video-chat-app/src/pages/videochat/VideoChat.js
+++ react-video-chat-app/src/pages/videochat/VideoChat.js
@@ -1,3 +1,5 @@
+import "./VideoChat.css";
+
 import {useEffect, useState, useRef, forwardRef} from 'react';
 import {useMqttState, useSubscription} from 'mqtt-react-hooks';
 import firebase from "firebase";
@@ -30,12 +32,10 @@ export default function VideoChat() {
 	const localVideoRef = useRef(null);
 	const remoteVideoRef = useRef(null);
 	const { client, connectionStatus } = useMqttState();
-	const { message } = useSubscription([topics.publishPresence, topics.publishOffer]);
-
-	/*useEffect(() => {
-		createCall();
-		//answerCall();
-	}, []);*/
+	const { message } = useSubscription([
+		topics.publishPresence,
+		topics.publishOffer,
+	]);
 
 	useEffect(() => {
 		if (connectionStatus.toLowerCase() === "connected") {
@@ -47,14 +47,7 @@ export default function VideoChat() {
 		if (message) {
 			if (message.message != meetingId && message.topic == topics.publishPresence) {
 				setMeetingId(message.message);
-				console.log("Creating offer");
-				/*createCall(message.message)
-					.then(() => {
-						publishOffer("offer published");
-						setHavePublishedOffer(true)
-					});*/
 			} else if (message.topic === topics.publishOffer) {
-				console.log(message.topic, message.message);
 				!havePublishedOffer && answerCall(meetingId);
 			}
 		}
@@ -62,90 +55,32 @@ export default function VideoChat() {
 
 	useEffect(() => {
 	    getLocalVideo();
-	  }, [localVideoRef]);
+		getRemoteVideo();
+	  }, [localVideoRef, remoteVideoRef]);
 
-	useEffect(() => {
-		remoteVideoRef.current && initializeRemoteVideo();
-	  }, [remoteVideoRef]);
-
-	const initializeRemoteVideo = async () => {
+	const getRemoteVideo = async () => {
 		remoteStream = new MediaStream();
 
 		pc.ontrack = (event) => {
-			console.log("hallo!");
 			event.streams[0].getTracks().forEach((track) => {
 				remoteStream.addTrack(track);
 			});
 		};
 
 		remoteVideoRef.current.srcObject = remoteStream;
-		console.log("remote video initialized");
-		/*remoteStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-
-		remoteStream.getTracks().forEach((track) => {
-			pc.addTrack(track, remoteStream);
-		});
-
-		remoteVideoRef.current.srcObject = remoteStream;*/
 	}
 	
-	const getLocalVideo = () => {
-		navigator
-			.mediaDevices
-			.getUserMedia({ video: true, audio: true })
-			.then((stream) => {
-				localStream = stream;
-				localStream.getTracks().forEach((track) => {
-					pc.addTrack(track, localStream);
-				});
-				let video = localVideoRef.current;
-				video.srcObject = stream;
-				video.play();
-			}).catch((error) => {
-				console.error(error);
-		})
+	const getLocalVideo = async () => {
+		localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+
+		localStream.getTracks().forEach((track) => {
+			pc.addTrack(track, localStream);
+		});
+
+		localVideoRef.current.srcObject = localStream;
 	}
 
 	async function createCall(meetingId) {
-		/*const callDoc = db.collection("calls").doc(meetingId);
-		const offerCandidates = callDoc.collection("offerCandidates");
-		const answerCandidates = callDoc.collection("answerCandidates");
-
-		pc.onicecandidate = (event) => {
-			event.candidate && offerCandidates.add(event.candidate.toJSON());
-		};
-
-		const offerDescription = await pc.createOffer();
-		await pc.setLocalDescription(offerDescription);
-
-		const offer = {
-			sdp: offerDescription.sdp,
-			type: offerDescription.type,
-		};
-
-		await callDoc.set({ offer });
-		publishOffer("offer published");
-
-		// Listen for remote answer
-		callDoc.onSnapshot((snapshot) => {
-			const data = snapshot.data();
-			console.log("callDocSnapshot", data);
-			if (!pc.currentRemoteDescription && data?.answer) {
-				const answerDescription = new RTCSessionDescription(data.answer);
-				pc.setRemoteDescription(answerDescription);
-			}
-		});
-
-		// Listen for remote ICE candidates
-		answerCandidates.onSnapshot((snapshot) => {
-			snapshot.docChanges().forEach((change) => {
-				console.log("answerCandidateSnapshot", change.doc.data());
-				if (change.type === "added") {
-					const candidate = new RTCIceCandidate(change.doc.data());
-					pc.addIceCandidate(candidate);
-				}
-			})
-		})*/
 		// Reference Firestore collections for signaling
 		const callDoc = db.collection('calls').doc(meetingId);
 		const offerCandidates = callDoc.collection('offerCandidates');
@@ -153,7 +88,6 @@ export default function VideoChat() {
 
 		// Get candidates for caller, save to db
 		pc.onicecandidate = (event) => {
-			console.log("an event occurred");
 			event.candidate && offerCandidates.add(event.candidate.toJSON());
 		};
 
@@ -167,6 +101,7 @@ export default function VideoChat() {
 		};
 
 		await callDoc.set({ offer });
+		publishOffer(JSON.stringify(offer));
 
 		// Listen for remote answer
 		callDoc.onSnapshot((snapshot) => {
@@ -182,7 +117,6 @@ export default function VideoChat() {
 			snapshot.docChanges().forEach((change) => {
 				if (change.type === 'added') {
 					const candidate = new RTCIceCandidate(change.doc.data());
-					console.log("adding answer candidate");
 					pc.addIceCandidate(candidate);
 				}
 			});
@@ -190,50 +124,11 @@ export default function VideoChat() {
 	}
 
 	async function answerCall(meetingId) {
-		console.log("answering...");
-		/*const callDoc = db.collection("calls").doc(meetingId);
-		const offerCandidates = callDoc.collection("offerCandidates");
-		const answerCandidates = callDoc.collection("answerCandidates");
-
-		pc.onicecandidate = (event) => {
-			console.log("heisann, ejf er en kandidat");
-			event.candidate && answerCandidates.add(event.candidate.toJSON());
-		};
-
-		// Fetch data, then set the offer and answer
-		const callData = (await callDoc.get()).data();
-
-		const offerDescription = callData.offer;
-		await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
-
-		const answerDescription = await pc.createAnswer();
-		await pc.setLocalDescription(answerDescription);
-
-		const answer = {
-			sdp: answerDescription.sdp,
-			type: answerDescription.type,
-		};
-
-		await callDoc.update({ answer });
-
-		// Listen to offer candidates
-
-		offerCandidates.onSnapshot((snapshot) => {
-			snapshot.docChanges().forEach((change) => {
-				console.log(change);
-				if (change.type === "added") {
-					const data = change.doc.data();
-					pc.addIceCandidate(new RTCIceCandidate(data));
-				}
-			})
-		})*/
-
 		const callDoc = db.collection('calls').doc(meetingId);
 		const answerCandidates = callDoc.collection('answerCandidates');
 		const offerCandidates = callDoc.collection('offerCandidates');
 
 		pc.onicecandidate = (event) => {
-			console.log("an event occurred");
 			event.candidate && answerCandidates.add(event.candidate.toJSON());
 		};
 
@@ -256,46 +151,56 @@ export default function VideoChat() {
 			snapshot.docChanges().forEach((change) => {
 				if (change.type === 'added') {
 					let data = change.doc.data();
-					console.log("adding offer candidate");
 					pc.addIceCandidate(new RTCIceCandidate(data));
 				}
 			});
 		});
 	}
 
+	/**
+	 * Publishes a message to the MQTT-broker on topic 'ttm4115/team_12/publishPresence'
+	 * @param message
+	 */
 	function publishPresence(message) {
 		if (connectionStatus.toLowerCase() === "connected") {
 			client.publish(topics.publishPresence, message);
 		}
 	}
 
+	/**
+	 * Publishes a message to the MQTT-broker on topic 'ttm4115/team_12/publishPresence'
+	 * @param message
+	 */
 	function publishOffer(offer) {
 		if (connectionStatus.toLowerCase() === "connected") {
 			client.publish(topics.publishOffer, offer);
 		}
 	}
 
-	function toBeDeleted() {
-		createCall(message.message)
+	/**
+	 * Creates a call
+	 */
+	function call() {
+		createCall(message.topic === topics.publishPresence ? message.message : meetingId)
 			.then(() => {
-				publishOffer("offer published");
 				setHavePublishedOffer(true)
 			});
 	}
 
 	return (<div>
-		<h1>Video chat application</h1>
-		<button onClick={toBeDeleted}>Klikk meg</button>
-		<VideoStream props={{ location: "webcamVideo" }} ref={localVideoRef} />
-		<VideoStream props={{ location: "remoteVideo" }} ref={remoteVideoRef} />
+		<h1 id="page-title">Video chat application</h1>
+		<button onClick={call} id={"call-btn"}>Click to call</button>
+		<div id="videostream-container" className="container-100">
+			<VideoStream props={{ location: "webcamVideo" }} ref={localVideoRef} />
+			<VideoStream props={{ location: "remoteVideo" }} ref={remoteVideoRef} />
+		</div>
 	</div>);
 }
 
 const VideoStream = forwardRef((props, ref) => {
 	return (
 		<>
-			<p>hei</p>
-			<video ref={ref} id={props.location} autoPlay playsInline/>
+			<video ref={ref} id={props.location} className="videostream" autoPlay playsInline/>
 		</>
 	);
 })
